5aecb13aa584ea8d37d93b49f3a0802dbe3f5322
mohamedosama2074 <iosengks74@gmail.com>
2019-08-26 00:13:04 +0200

    UIActivityViewController_Share With All apps

--- UIActivityViewController/ViewController.swift
+++ UIActivityViewController/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textShare: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btn_Share(_ sender: UIButton) {
+        
+        
+        guard let share = textShare.text else {
+            return
+        }
+        
+        let activity = UIActivityViewController(activityItems: [share], applicationActivities: nil)
+        
+        present(activity, animated: true, completion: nil)
+    }
 }
 
